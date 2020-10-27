843154ec154c073dd6aa757ff8afca4f291692e4
jungrae.cho <chojl1125@gmail.com>
2020-10-27 18:03:14 +0900

    feat: 최초 커밋 closed #104

---
+++ IssueTracker/IssueTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IssueTracker
+//
+//  Created by 조정래 on 2020/10/27.
+//  Copyright © 2020 ralph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
