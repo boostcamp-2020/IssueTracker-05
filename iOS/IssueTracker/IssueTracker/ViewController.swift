24091c8a68f89907739e56433313e911edc03016
jjm159 <wjdwoaud15@gmail.com>
2020-10-27 20:04:23 +0900

    chore: 주석 제거

--- iOS/IssueTracker/IssueTracker/ViewController.swift
+++ iOS/IssueTracker/IssueTracker/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  IssueTracker
-//
-//  Created by 조정래 on 2020/10/27.
-//  Copyright © 2020 ralph. All rights reserved.
-//
 
 import UIKit
 
@@ -12,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 }
