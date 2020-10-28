1a87930e5239b5c2354112da4a30b85cb7489234
seokminKang <soos0228@naver.com>
2020-10-28 11:17:12 +0900

    chore : 백엔드 개발환경 삭제

--- server/src/api/controllers/login.js
+++
@@ -1,5 +0,0 @@
-const login = (req, res) => {
-  res.send('Hello World!');
-};
-
-export default { login };
--- server/src/loaders/index.js
+++
@@ -1,6 +0,0 @@
-import express from './express';
-
-export default async (app) => {
-  await express(app);
-  console.log('🍟 Express Connect! 🍟');
-};
