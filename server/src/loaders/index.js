dd582cb772d8ed5ebc0498751d6bfca41931d70d
hansanguk0222 <hansanguk0222@gmail.com>
2020-10-28 16:48:02 +0900

    feat: db 초기 설정

--- server/src/loaders/index.js
+++ server/src/loaders/index.js
@@ -1,6 +1,10 @@
 import express from './express';
+import mysql from './mysql';
 
 export default async (app) => {
+  await mysql();
+  console.log('🎉 DB Connect! 🎉');
+
   await express(app);
   console.log('🍟 Express Connect! 🍟');
 };
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,31 +1,41 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+import dbConfig from '../config/dbConfig';
+const config = dbConfig[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    port: config.port,
+    dialect: 'mysql',
+    timezone: '+09:00',
+  },
+);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))['default'](
+      sequelize,
+      Sequelize.DataTypes,
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
