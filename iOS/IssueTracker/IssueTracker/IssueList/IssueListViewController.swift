59eb5ba88eb70aac4a15c195b54bf9d727b851f0
jjm159 <wjdwoaud15@gmail.com>
2020-10-28 17:54:38 +0900

    feat: IssueListViewController Preview 설정

--- iOS/IssueTracker/IssueTracker/IssueList/IssueListViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/IssueListViewController.swift
@@ -2,11 +2,25 @@
 import UIKit
 
 class IssueListViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
     }
     
+    
 }
+
+#if DEBUG
+
+import SwiftUI
+
+struct Preview: PreviewProvider {
+    static var previews: some View {
+        let vc = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(identifier: "IssueListViewController")
+        return vc.view.liveView
+    }
+}
+
+#endif
