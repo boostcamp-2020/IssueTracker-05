b3f4da93c285c957b37335f745fef97f77cf6981
jjm159 <wjdwoaud15@gmail.com>
2020-10-28 22:48:27 +0900

    feat: SearchListViewController 추가

--- iOS/IssueTracker/IssueTracker/IssueList/IssueListMainViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/IssueListMainViewController.swift
@@ -3,14 +3,27 @@ import UIKit
 
 class IssueListMainViewController: UIViewController {
     
+    @IBOutlet weak var containerView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-        let resultViewController = storyboard!.instantiateViewController(identifier: "IssueResultViewController")
-
-        present(resultViewController, animated: true)
+//        let searchListViewController = storyboard!.instantiateViewController(identifier: "SearchListViewController")
+//
+//        let nav = storyboard!.instantiateViewController(identifier: "issueListNavigation")
+//        as UINavigationController
+//
+//        nav.pushViewController(searchListViewController, animated: false)
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let searchListViewController = storyboard!.instantiateViewController(identifier: "SearchListViewController")
+        
+        let nav = storyboard!.instantiateViewController(identifier: "issueListNavigation")
+        as UINavigationController
+        
+        nav.pushViewController(searchListViewController, animated: false)
     }
     
     
