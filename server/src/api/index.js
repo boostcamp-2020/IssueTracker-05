61efc6f61d581f11832b7f955e8451e19d38e139
seokminKang <soos0228@naver.com>
2020-10-29 15:26:27 +0900

    feat: signup 기능 Express에 추가

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import login from './routes/login';
+import signup from '@api/routes/signup';
 
 export default () => {
   const app = Router();
 
   login(app);
+  signup(app);
 
   return app;
 };
