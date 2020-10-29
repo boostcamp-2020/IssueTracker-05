cf571052137a683fc32b6cea8a22865f5742e04b
jjm159 <wjdwoaud15@gmail.com>
2020-10-29 11:39:14 +0900

    feat: search 시작하면 searchList로 뷰 전환
    searchBarDelegate를 사용하여 searchBarShouldBeginEditing에서 searchContrainerView.isHidden을 false로 바꿔주었다. 그리고 searchBarTextDidEndEditing에서 다시 true로 바꿔주었다.

--- iOS/IssueTracker/IssueTracker/IssueList/View/Main/IssueListMainViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/View/Main/IssueListMainViewController.swift
@@ -1,18 +1,44 @@
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class IssueListMainViewController: UIViewController {
     
-    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var resultContrainerView: UIView!
+    @IBOutlet weak var searchContrainerView: UIView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var viewModel = IssueListViewModel()
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        searchBar.delegate = self
+        searchContrainerView.isHidden = true
         
         
     }
     
+}
+
+extension IssueListMainViewController: UISearchBarDelegate {
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchContrainerView.isHidden = false
+        return true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) { // 엔터 치면 작동
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchContrainerView.isHidden = true
+    }
     
 }
+
 
 #if DEBUG
 
