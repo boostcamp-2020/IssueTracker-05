d851ceae8e9d12d2ba7bdeb5826d0d6b72415def
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-02 18:19:26 +0900

    feat: 댓글 전체 불러오는 기능 추가

--- server/src/api/controllers/comment.js
+++ server/src/api/controllers/comment.js
@@ -1,9 +1,20 @@
 import commentService from '@services/comment';
 
-const createComment = async (req, res) => {
-  const { nickname, content } = req.body;
+const getComments = async (req, res) => {
+  const { iid } = req.params;
   try {
-    await commentService.createComment(nickname, content);
+    const comments = await commentService.getComments(iid);
+    return res.status(200).json(comments);
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const createComment = async (req, res) => {
+  const { nickname, content, iid } = req.body;
+  const { uid } = req.user;
+  try {
+    await commentService.createComment(nickname, content, uid, iid);
     return res.status(200).end();
   } catch (err) {
     return res.status(400).json({
@@ -29,6 +40,7 @@ const deleteComment = async (req, res) => {
   const { cid } = req.params;
   try {
     await commentService.deleteComment(cid);
+    return res.status(200).end();
   } catch (err) {
     return res.status(400).json({
       message: err.message,
@@ -36,4 +48,4 @@ const deleteComment = async (req, res) => {
   }
 };
 
-export default { createComment, patchComment, deleteComment };
+export default { getComments, createComment, patchComment, deleteComment };
--- server/src/api/routes/comment.js
+++ server/src/api/routes/comment.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 
 import commentController from '@api/controllers/comment';
+import passportJwtMiddleware from '@api/middlewares/passportJwt';
 
 export default (app) => {
   const route = Router();
 
   app.use('/comment', route);
 
+  route.use(passportJwtMiddleware);
+
+  route.get('/:iid', commentController.getComments);
   route.post('/', commentController.createComment);
-  route.patch('/:comment', commentController.patchComment);
-  route.delete('/:comment', commentController.deleteComment);
+  route.patch('/:cid', commentController.patchComment);
+  route.delete('/:cid', commentController.deleteComment);
 };
