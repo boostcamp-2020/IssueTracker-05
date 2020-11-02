60404951015503c36c81d49c7c9c24c4006e4110
Mong-Gu <yhw8258@naver.com>
2020-11-02 17:58:43 +0900

    feat: app에 issue router 등록

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import signup from '@api/routes/signup';
 import login from '@api/routes/login';
 import milestone from '@api/routes/milestone';
+import issue from '@api/routes/issue';
 
 export default () => {
   const app = Router();
@@ -10,6 +11,7 @@ export default () => {
   login(app);
   signup(app);
   milestone(app);
+  issue(app);
 
   return app;
 };
