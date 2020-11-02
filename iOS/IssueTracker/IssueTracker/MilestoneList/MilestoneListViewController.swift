24692cd228604b41b42cd7cef2db8f39b6d062e2
jungrae.cho <chojl1125@gmail.com>
2020-11-03 08:59:07 +0900

    feat: 마일스톤 셀 구성

--- iOS/IssueTracker/IssueTracker/MilestoneList/MilestoneListViewController.swift
+++ iOS/IssueTracker/IssueTracker/MilestoneList/MilestoneListViewController.swift
@@ -1,20 +1,5 @@
 import UIKit
 
-
-class Cell: Hashable {
-    
-    var id:String?
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    static func == (lhs: Cell, rhs: Cell) -> Bool {
-        lhs.id == rhs.id
-    }
-    
-}
-
 enum Section {
     case main
 }
@@ -23,24 +8,45 @@ class MilestoneListViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var datasource: UICollectionViewDiffableDataSource<Section, MilestoneListModel>!
+    
+    //MARK: - Value Types
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, MilestoneListModel>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, MilestoneListModel>
+    
+    private lazy var dataSource = makeDataSource()
     
     let itemList = [MilestoneListModel(id: "1"), MilestoneListModel(id: "2")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.collectionView.register(MilestoneListViewCell.self, forCellWithReuseIdentifier: "MilestoneListViewCell")
-        
-        self.datasource = .init(collectionView: self.collectionView, cellProvider: { collectionView, indexPath, item -> UICollectionViewCell? in
-                                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MilestoneListViewCell", for: indexPath) as! MilestoneListViewCell
-                                    cell.id = "11"
-                                    return cell })
     }
     
+    func makeDataSource() -> DataSource {
+        // 1
+        let dataSource = DataSource(
+            collectionView: collectionView,
+            cellProvider: { (collectionView, indexPath, item) ->
+                UICollectionViewCell? in
+                // 2
+                let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: "MilestoneListViewCell",
+                    for: indexPath) as? MilestoneListViewCell
+                cell?.id = item.id
+                return cell
+            })
+        return dataSource
+    }
     
-    func insertSomeThing() {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, MilestoneListViewCell>()
+    func applySnapshot(animatingDifferences: Bool = true) {
+        // 2
+        var snapshot = Snapshot()
+        // 3
+        snapshot.appendSections([.main])
+        // 4
+        snapshot.appendItems(itemList, toSection: .main)
+        // 5
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
     }
     
     
