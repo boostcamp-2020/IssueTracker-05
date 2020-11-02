164c3a1249a0e930581c3d600d5f00aa012bbb5e
seokminKang <soos0228@naver.com>
2020-11-02 22:14:17 +0900

    feat: issue 추가기능 구현 (update issue 미구현)

--- server/src/api/routes/issue.js
+++ server/src/api/routes/issue.js
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 
 import issueController from '@api/controllers/issue';
+import passportJwtMiddleware from '@api/middlewares/passportJwt';
 
 export default (app) => {
   const route = Router();
 
   app.use('/issue', route);
 
-//   route.get('/', issueController.getIssues);
-  route.get('/:id', issueController.getIssue);
-//   route.post('/', issueController.createIssue);
-//   route.patch('/:id', issueController.updateIssue);
-//   route.delete('/:id', issueController.deleteIssue);
+  route.use(passportJwtMiddleware);
+
+  route.get('/', issueController.getIssues);
+  route.get('/:id', issueController.getTargetIssue);
+  route.post('/', issueController.createIssue);
+  // route.patch('/:id', issueController.updateIssue);
+  route.delete('/:id', issueController.deleteIssue);
 };
