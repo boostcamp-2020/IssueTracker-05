a9890ebf861319f00d7282720f801a534cb7a8b8
seokminKang <soos0228@naver.com>
2020-11-02 16:58:34 +0900

    feat: merge 병합

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import signup from '@api/routes/signup';
 import login from '@api/routes/login';
 import milestone from '@api/routes/milestone';
+import comment from '@api/routes/comment';
+import label from '@api/routes/label';
 
 export default () => {
   const app = Router();
@@ -10,6 +12,8 @@ export default () => {
   login(app);
   signup(app);
   milestone(app);
+  comment(app);
+  label(app);
 
   return app;
 };
