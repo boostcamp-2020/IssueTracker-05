31797b5d4d3554ba86361415b29175b3c34c0405
Mong-Gu <yhw8258@naver.com>
2020-11-03 20:49:28 +0900

    fix: user의 특정 정보만 받을 수 있도록 수정

--- server/src/services/user.js
+++ server/src/services/user.js
@@ -2,7 +2,9 @@ import db from '@models';
 
 const getUsers = async () => {
   try {
-    const users = db.user.findAll();
+    const users = db.user.findAll({
+      attributes: ['uid', 'userId', 'nickname'],
+    });
     return users;
   } catch (err) {
     throw new Error(err);
