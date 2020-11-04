7b66209c7c181c92af3dfd24cfd7898c2697600c
seokminKang <soos0228@naver.com>
2020-11-04 12:57:37 +0900

    fix: passport 경로 수정

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -10,10 +10,10 @@ export default {
   secretOrKey: process.env.JWT_SECRET_KEY,
   githubClientId: process.env.GITHUB_ID,
   githubClientSecret: process.env.GITHUB_SECRET,
-  githubCallbackURL: `http://localhost:${process.env.PORT}/${process.env.GITHUB_CALLBACK_URL}`,
+  githubCallbackURL: process.env.GITHUB_CALLBACK_URL,
   appleClientId: process.env.APPLE_CLIENT_ID,
   appleTeamId: process.env.APPLE_TEAM_ID,
   appleKeyId: process.env.APPLE_KEY_ID,
   applePrivateKey: process.env.APPLE_PRIVATE_KEY,
-  appleCallbackURL: `http://localhost:${process.env.PORT}/${process.env.APPLE_CALLBACK_URL}`,
+  appleCallbackURL: process.env.APPLE_CALLBACK_URL,
 };
