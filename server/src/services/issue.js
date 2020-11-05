eb12a61ec7853c1719745f4f3995a1ce827121a6
Mong-Gu <yhw8258@naver.com>
2020-11-06 00:11:55 +0900

    fix: closedAt 속성을 추가하면서 처리해야 할 사항 처리
    - isOpen 속성이 false로 되어 issue가 닫히게 되면 closedAt에 반영해주는 코드 작성

--- server/src/services/issue.js
+++ server/src/services/issue.js
@@ -171,6 +171,9 @@ const updateIssue = async (updatedContent, iid) => {
   try {
     if (updatedContent) {
       // update된 내용이 존재하면, 관계 테이블 관련 내용 제외하고 issue 기본 내용 수정
+      if (updatedContent.isOpen === false) {
+        updatedContent.closedAt = new Date();
+      }
       await db.issue.update(
         {
           ...updatedContent,
