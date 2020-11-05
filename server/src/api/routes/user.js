6228b702f76a3669371da2a7184a2dbc14b73a71
seokminKang <soos0228@naver.com>
2020-11-05 23:45:09 +0900

    fix: 유저 라우터 인증 추가

--- server/src/api/routes/user.js
+++ server/src/api/routes/user.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import userController from '@api/controllers/user';
+import passportJwt from '@api/middlewares/passportJwt';
 
 export default (app) => {
   const route = Router();
 
   app.use('/user', route);
+
+  route.use(passportJwt);
 
   route.get('/', userController.getUsers);
 };
