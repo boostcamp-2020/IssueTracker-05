8fc64ae5ee14810b029b75574b141c26c73fab42
jjm159 <wjdwoaud15@gmail.com>
2020-11-05 16:13:57 +0900

    feat: viewmodel, IssueListViewController 바인딩 추가
    - bind 함수로 바인딩 로직 분리
    - IssueResultViewController의 close, delete 버튼 클로저 변수에 ViewModel의 action 바인딩

--- iOS/IssueTracker/IssueTracker/IssueList/ViewModel/IssueListViewModel.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/ViewModel/IssueListViewModel.swift
@@ -6,14 +6,16 @@ class IssueListViewModel {
     
     struct Status {
         var issues =  Bindable(IssueListModel.all())
-        var searchResultTitleList = Bindable([String]())        // searchTitleList의 apply 바인딩
-        var searchResultList = Bindable(IssueListModel.all())   // searchResultLis의 apply 바인딩
+        var searchResultTitleList = Bindable([String]())
+        var searchResultList = Bindable(IssueListModel.all())
     }
     
     struct Action {
         var searchTextChanged: (String) -> Void
         var searchButtonClicked: (String) -> Void
         var searchCancelButtonClicked: () -> Void
+        var closeButtonTabbed: (Int) -> Void
+        var deleteButtonTabbed: (Int) -> Void
     }
     
     var status = Status()
@@ -40,6 +42,10 @@ class IssueListViewModel {
             weakSelf.status.searchResultList.value
                 = weakSelf.status.issues.value
             weakSelf.requestIssueData()
+        }, closeButtonTabbed: { iid in
+            print(iid, "close")
+        }, deleteButtonTabbed: { iid in
+            print(iid, "delete")
         })
     
     let url = "http://group05issuetracker.duckdns.org:49203/api/issue"
