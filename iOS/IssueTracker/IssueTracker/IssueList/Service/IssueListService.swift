a9f82b78de07c89e0ae34d3ced1aa0888d59c865
jjm159 <wjdwoaud15@gmail.com>
2020-11-06 02:56:35 +0900

    fix: issueList close 요청 API service 함수 수정
    - patch 방식으로 변경
    - parameter 데이터 수정

--- iOS/IssueTracker/IssueTracker/IssueList/Service/IssueListService.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/Service/IssueListService.swift
@@ -8,10 +8,19 @@ class IssueListService {
     
     // 마지막 값만 이넘값으로 수정하자. -  이넘 값들 common Service에 모아도 될듯!
     
-    let issueAPIURL = "http://group05issuetracker.duckdns.org:49203/api/issue"
+    let port: Int = 49203
+    lazy var issueAPIURL = "http://group05issuetracker.duckdns.org:\(port)/api/issue/"
  
-    let httpHeaders:HTTPHeaders = ["Accept": "application/json","Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjExLCJ1c2VySWQiOiJhYWExMTEiLCJwYXNzd29yZCI6IiQyYiQxMiRFQmIxMDBuckJCRTZZSDhXSWNvam5PS3d5YlYzSUQ1VHE4U2x6RWxnRWdXUm0xRU9TOVQzMiIsIm5pY2tuYW1lIjoiYWFhMTExIiwiT0F1dGgiOmZhbHNlLCJyZXNvdXJjZVNlcnZlciI6ImxvY2FsIiwiY3JlYXRlZEF0IjoiMjAyMC0xMS0wM1QwODozMzozNy4wMDBaIiwidXBkYXRlZEF0IjoiMjAyMC0xMS0wM1QwODozMzozNy4wMDBaIiwiaWF0IjoxNjA0NDgxMTIzfQ.p4nu83YEYOTD5NB7XKkqJTyAlgKDu6EPVgLMQqHQ_RY"]
+//    let httpHeaders:HTTPHeaders = ["Accept": "application/json",
+//                                   "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjExLCJ1c2VySWQiOiJhYWExMTEiLCJwYXNzd29yZCI6IiQyYiQxMiRFQmIxMDBuckJCRTZZSDhXSWNvam5PS3d5YlYzSUQ1VHE4U2x6RWxnRWdXUm0xRU9TOVQzMiIsIm5pY2tuYW1lIjoiYWFhMTExIiwiT0F1dGgiOmZhbHNlLCJyZXNvdXJjZVNlcnZlciI6ImxvY2FsIiwiY3JlYXRlZEF0IjoiMjAyMC0xMS0wM1QwODozMzozNy4wMDBaIiwidXBkYXRlZEF0IjoiMjAyMC0xMS0wM1QwODozMzozNy4wMDBaIiwiaWF0IjoxNjA0NDgxMTIzfQ.p4nu83YEYOTD5NB7XKkqJTyAlgKDu6EPVgLMQqHQ_RY"]
     
+    let httpHeaders:HTTPHeaders = ["Accept": "application/json",
+                                       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE3LCJ1c2VySWQiOiJ0ZXN0SWQxIiwicGFzc3dvcmQiOiIkMmIkMTIkME4ucWNLU09XYm9EcFI1RjZnenNydTd5L0pqV3VtTnRmUE02Tjl6ZTZ6NDBFQkxvdUNHc3kiLCJuaWNrbmFtZSI6InRlc3ROaWNrbmFtZTEiLCJPQXV0aCI6ZmFsc2UsInJlc291cmNlU2VydmVyIjoibG9jYWwiLCJjcmVhdGVkQXQiOiIyMDIwLTExLTA1VDEyOjE3OjA5LjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTExLTA1VDEyOjE3OjA5LjAwMFoiLCJpYXQiOjE2MDQ1NzkwNDF9.Jq_zpkUT9VejHkwrtDh8Sa1uRguV82Mw6uorKViBPfg"]
+    
+    //
+        
+        
+        
     unowned var viewModel: IssueListViewModel
     
     init(viewModel: IssueListViewModel){
@@ -27,9 +36,15 @@ class IssueListService {
             switch response.result {
             case .success(let result):
                 do {
+                    
+                    print(result)
+                    
                     let resultData = try JSONSerialization.data(withJSONObject: result, options: .prettyPrinted)
+                    print(resultData)
+                    
                     let decodedData = try JSONDecoder().decode([IssueListModel].self, from: resultData)
                     weakSelf.viewModel.status.issues.value = decodedData
+                    
                 } catch {
                     print(error)
                 }
@@ -41,21 +56,12 @@ class IssueListService {
     
     //MARK: POST
     func requestCloaseIssue(issueId: Int) {
-        
-        var newIssue: IssueListModel?
-        
-        for index in viewModel.status.issues.value.indices {
-            if viewModel.status.issues.value[index].iid == issueId {
-                newIssue = viewModel.status.issues.value[index]
-            }
-        }
-        
-        guard var issue = newIssue else { return }
-        issue.isOpen = false
+
+        let issueParameter = ["isOpen": "\(false)"]
         
         AF.request(issueAPIURL + "\(issueId)",
-                   method: .post,
-                   parameters: issue,
+                   method: .patch,
+                   parameters: issueParameter,
                    headers: httpHeaders)
             .responseData { response in
                 switch response.result {
@@ -76,6 +82,7 @@ class IssueListService {
                 guard let weakSelf = self else { return }
                 switch response.result {
                 case .success(let data):
+                    print(String(data: data, encoding: .utf8))
                     weakSelf.requestIssueData()
                     print("success",data)
                 case .failure(let error):
