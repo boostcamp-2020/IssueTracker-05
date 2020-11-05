ff217bb337cb6068a7747b87f7b6dfb527df4cbd
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-05 16:43:51 +0900

    feat: atom 단계에서 Menu 요소를 추가

---
+++ client/src/components/atoms/Menu/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { font, palette } from "styled-theme";
+
+const backgroundColor = ({ MenuBckground }) => palette(MenuBackground);
+const fontSize = ({ MenuFontSize }) => `${MenuFontSize}px`;
+const setHeight = ({ MenuHeight }) => `${MenuHeight}px`;
+const setWidth = ({ MenuWidth }) => `${MenuWidth}px`;
+
+const StyledMenu = styled.div`
+  box-sizing: border-box;
+  font-size: ${fontSize};
+  font-family: ${font("primary")};
+  background: ${backgroundColor};
+  height: ${setHeight};
+  width: ${setWidth};
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Menu = (props) => {
+  return <StyledMenu {...props} />;
+};
+
+Menu.PropTypes = {
+  MenuBackground: PropTypes.number,
+  MenuSize: PropTypes.number,
+  MenuHeight: PropTypes.number,
+  MenuWidth: PropTypes.number,
+};
+
+Menu.defaultProps = {
+  palette: "primary",
+};
+
+export default Menu;
