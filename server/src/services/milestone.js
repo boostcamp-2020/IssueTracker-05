dcab74718dff05a028a5ea39317523ee464a7c94
seokminKang <soos0228@naver.com>
2020-11-06 10:07:46 +0900

    fix: 마일스톤 undefined/null을 받아오던 점 수정

--- server/src/services/milestone.js
+++ server/src/services/milestone.js
@@ -31,7 +31,7 @@ const deleteMilestoneFromDB = async (milestoneId) => {
 
 const editMilestoneFromDB = async (milestoneId, content) => {
   try {
-    if (!content.isOpen) {
+    if (content.isOpen === false) {
       content.closedAt = new Date();
     }
     const targetMilestone = await db.milestone.update(content, {
