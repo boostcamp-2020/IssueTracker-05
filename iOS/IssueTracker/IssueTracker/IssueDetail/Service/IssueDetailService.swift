c92919b7fc242837ea5642d90ea78aeffc2ace5d
jjm159 <wjdwoaud15@gmail.com>
2020-11-07 13:20:16 +0900

    fix: Bearer 토큰 -> 함수로 변경

--- iOS/IssueTracker/IssueTracker/IssueDetail/Service/IssueDetailService.swift
+++ iOS/IssueTracker/IssueTracker/IssueDetail/Service/IssueDetailService.swift
@@ -4,14 +4,16 @@ import Alamofire
 class IssueDetailService {
     
     let url = "http://group05issuetracker.duckdns.org:49203"
-//    let headers: HTTPHeaders = ["Accept": "application/json","Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjExLCJ1c2VySWQiOiJhYWExMTEiLCJwYXNzd29yZCI6IiQyYiQxMiRFQmIxMDBuckJCRTZZSDhXSWNvam5PS3d5YlYzSUQ1VHE4U2x6RWxnRWdXUm0xRU9TOVQzMiIsIm5pY2tuYW1lIjoiYWFhMTExIiwiT0F1dGgiOmZhbHNlLCJyZXNvdXJjZVNlcnZlciI6ImxvY2FsIiwiY3JlYXRlZEF0IjoiMjAyMC0xMS0wM1QwODozMzozNy4wMDBaIiwidXBkYXRlZEF0IjoiMjAyMC0xMS0wM1QwODozMzozNy4wMDBaIiwiaWF0IjoxNjA0NDgxMTIzfQ.p4nu83YEYOTD5NB7XKkqJTyAlgKDu6EPVgLMQqHQ_RY"]
     
-    let headers: HTTPHeaders = ["Accept": "application/json","Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE3LCJ1c2VySWQiOiJ0ZXN0SWQxIiwicGFzc3dvcmQiOiIkMmIkMTIkME4ucWNLU09XYm9EcFI1RjZnenNydTd5L0pqV3VtTnRmUE02Tjl6ZTZ6NDBFQkxvdUNHc3kiLCJuaWNrbmFtZSI6InRlc3ROaWNrbmFtZTEiLCJPQXV0aCI6ZmFsc2UsInJlc291cmNlU2VydmVyIjoibG9jYWwiLCJjcmVhdGVkQXQiOiIyMDIwLTExLTA1VDEyOjE3OjA5LjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTExLTA1VDEyOjE3OjA5LjAwMFoiLCJpYXQiOjE2MDQ1NzkwNDF9.Jq_zpkUT9VejHkwrtDh8Sa1uRguV82Mw6uorKViBPfg"]
+    let headers: HTTPHeaders
     
     unowned let viewModel: IssueDetailViewModel
     
     init(viewModel: IssueDetailViewModel) {
         self.viewModel = viewModel
+        headers
+            = ["Accept": "application/json",
+               "Authorization": "Bearer \(UserDefaults.standard.string(forKey: "token")!)"]
     }
     
     func callAPI(with issueId: Int) {
--- iOS/IssueTracker/IssueTracker/MilestoneList/VIewModel/MilestoneListViewModel.swift
+++ iOS/IssueTracker/IssueTracker/MilestoneList/VIewModel/MilestoneListViewModel.swift
@@ -21,9 +21,12 @@ class MilestoneListViewModel {
     var status = Status()
     
     let url = "http://group05issuetracker.duckdns.org:49203/api/milestone"
-    let httpHeaders:HTTPHeaders = ["Accept": "application/json","Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjExLCJ1c2VySWQiOiJhYWExMTEiLCJwYXNzd29yZCI6IiQyYiQxMiRFQmIxMDBuckJCRTZZSDhXSWNvam5PS3d5YlYzSUQ1VHE4U2x6RWxnRWdXUm0xRU9TOVQzMiIsIm5pY2tuYW1lIjoiYWFhMTExIiwiT0F1dGgiOmZhbHNlLCJyZXNvdXJjZVNlcnZlciI6ImxvY2FsIiwiY3JlYXRlZEF0IjoiMjAyMC0xMS0wM1QwODozMzozNy4wMDBaIiwidXBkYXRlZEF0IjoiMjAyMC0xMS0wM1QwODozMzozNy4wMDBaIiwiaWF0IjoxNjA0NDgxMTIzfQ.p4nu83YEYOTD5NB7XKkqJTyAlgKDu6EPVgLMQqHQ_RY"]
+    let httpHeaders:HTTPHeaders
     
     init() {
+        httpHeaders
+            = ["Accept": "application/json",
+               "Authorization": "Bearer \(UserDefaults.standard.string(forKey: "token")!)"]
         requestMilestoneData()
     }
         
