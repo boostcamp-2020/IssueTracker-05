c83f3c12c448397e0cda0dd74ea6ea05be5f7583
seokminKang <soos0228@naver.com>
2020-11-07 16:02:12 +0900

    style: route 스타일 변경

--- server/src/api/middlewares/comment.js
+++ server/src/api/middlewares/comment.js
@@ -4,7 +4,7 @@ const checkWritenUser = async (req, res, next) => {
   const { cid } = req.params;
   const { uid } = req.user;
   try {
-    const comment = await commentService.getComment(cid);
+    const comment = await commentService.readComment(cid);
     if (comment.uid !== uid) {
       return res.status(400).json({
         message: '댓글을 쓴 사용자가 아닙니다.',
