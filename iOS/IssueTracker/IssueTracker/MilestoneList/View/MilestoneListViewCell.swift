abcfcd011b70dc78c3162b92ec612cb76508c2c8
jjm159 <wjdwoaud15@gmail.com>
2020-11-09 02:31:04 +0900

    feat: milestoneList 더미데이터로 화면 출력
    - 뷰모델 초기 값에 더미 데이터 주입
    - 컴포지셔널, 디퍼블 코드 추가
    - cell title 버튼 이름 변경
    - cell setup 함수 추가

--- iOS/IssueTracker/IssueTracker/MilestoneList/View/MilestoneListViewCell.swift
+++ iOS/IssueTracker/IssueTracker/MilestoneList/View/MilestoneListViewCell.swift
@@ -1,22 +1,19 @@
-//
-//  MilestoneListViewCell.swift
-//  IssueTracker
-//
-//  Created by cho on 2020/11/02.
-//  Copyright © 2020 ralph. All rights reserved.
-//
 
-import Foundation
 import UIKit
+
 class MilestoneListViewCell: UICollectionViewCell {
     
+    @IBOutlet var milestoneTitle: UIButton!
     @IBOutlet var dueDateLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     @IBOutlet var openLabel: UILabel!
     @IBOutlet var closedLabel: UILabel!
     @IBOutlet var percentageLabel: UILabel!
-    @IBOutlet var sprintButton: UIButton!
     
-    
+    func setup(title: String, open: Int, close: Int) {
+        milestoneTitle.setTitle(title, for: .normal)
+        openLabel.text = "\(open)"
+        closedLabel.text = "\(close)"
+    }
     
 }
--- iOS/IssueTracker/IssueTracker/MilestoneList/View/MilestoneListViewController.swift
+++ iOS/IssueTracker/IssueTracker/MilestoneList/View/MilestoneListViewController.swift
@@ -4,11 +4,55 @@ import UIKit
 class MilestoneListViewController: UIViewController {
     
     var didSendEventClosure: ((MilestoneListViewController.Event)-> Void)?
+    lazy var dataLayout = makeDataLayout()
+    var viewModel = MilestoneListViewModel()
+    
     @IBOutlet weak var collectionView: UICollectionView!
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        collectionView.collectionViewLayout
+            = configureCollectionViewLayout()
+        bind()
+    }
+    
+    func bind() {
+        viewModel.status.milestones.bindAndFire(applyAnapshot)
+    }
+    
+    func applyAnapshot(sections: [Milestone]) {
+        var snapshot = NSDiffableDataSourceSnapshot<[Milestone], Milestone>()
+        snapshot.appendSections([sections])
+        snapshot.appendItems(sections)
+        dataLayout.apply(snapshot, animatingDifferences: true)
+    }
+    
+    func configureCollectionViewLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(80))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: itemSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    func makeDataLayout() -> UICollectionViewDiffableDataSource<[Milestone],Milestone> {
+        return UICollectionViewDiffableDataSource<[Milestone], Milestone>(
+            collectionView: collectionView,
+            cellProvider: { [weak self] collectionView, indexPath, milestone
+                -> UICollectionViewCell? in
+                guard let weakSelf = self else { return nil }
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MilestoneListViewCell", for: indexPath) as? MilestoneListViewCell else {
+                    return nil
+                }
+                cell.setup(
+                    title: milestone.title,
+                    open: milestone.issues.open,
+                    close: milestone.issues.close)
+                return cell
+            })
     }
 
 }
