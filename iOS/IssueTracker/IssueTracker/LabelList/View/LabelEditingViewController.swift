211598b1cd67d1e3933a0320adc674d5a3996a3f
jjm159 <wjdwoaud15@gmail.com>
2020-11-08 18:10:28 +0900

    feat: label edit popup 창 레이아웃 수정

--- iOS/IssueTracker/IssueTracker/LabelList/View/LabelEditingViewController.swift
+++ iOS/IssueTracker/IssueTracker/LabelList/View/LabelEditingViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class LabelEditingViewController: UIViewController {
     
+//    @IBOutlet weak var title: UILabel!
+//    @IBOutlet weak var title: UILabel!
+//    @IBOutlet weak var title: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.gray.withAlphaComponent(0.8)
