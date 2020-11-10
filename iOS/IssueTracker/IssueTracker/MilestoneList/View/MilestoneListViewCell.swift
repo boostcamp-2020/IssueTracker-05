f4563bafd284c078266ff496510e87810ceb6df9
jjm159 <wjdwoaud15@gmail.com>
2020-11-10 13:50:30 +0900

    feat: 마일스톤 셀 레이아웃 설정
    - 이전 커밋에서 마일스톤 타이틀 버튼 인셋 추가
    - 타이틀, 날짜 글씨 크기 작게 조정
    - 타이틀 compression 우선순위 1 +, 크기 조정시 날짜 텍스트만 줄어들도록

--- iOS/IssueTracker/IssueTracker/MilestoneList/View/MilestoneListViewCell.swift
+++ iOS/IssueTracker/IssueTracker/MilestoneList/View/MilestoneListViewCell.swift
@@ -13,6 +13,22 @@ class MilestoneListViewCell: UICollectionViewCell {
     
     var issueStates: [IssueState]?
     
+    
+    func setup(with milestone: Milestone) {
+        milestoneTitle.setTitle(milestone.title, for: .normal)
+        milestoneTitle.contentEdgeInsets.top = 2
+        milestoneTitle.contentEdgeInsets.bottom = 2
+        milestoneTitle.contentEdgeInsets.left = 4
+        milestoneTitle.contentEdgeInsets.right = 4
+        dueDateLabel.text = milestone.updatedAt
+        // TODO: 날짜 형식 바꿔주기
+        descriptionLabel.text = milestone.content ?? ""
+        issueStates = milestone.issues
+        percentageLabel.text = "\(percentageProgress)%"
+        openLabel.text = "\(numberOfOpen ?? 0) open"
+        closedLabel.text = "\(numberOfOpen ?? 0) closed"
+    }
+    
     var numberOfOpen: Int? {
         guard let issueStates = self.issueStates else { return nil }
         return issueStates.filter { $0.isOpen == true }.count
@@ -28,18 +44,6 @@ class MilestoneListViewCell: UICollectionViewCell {
         guard let close = numberOfClosed else { return 0 }
         if open + close == 0 { return 0 }
         return Int(Float(open) / Float(open + close))
-    }
-    
-    func setup(with milestone: Milestone) {
-        milestoneTitle.setTitle(milestone.title, for: .normal)
-        dueDateLabel.text = milestone.title
-        descriptionLabel.text = milestone.content ?? ""
-        issueStates = milestone.issues
-        
-        percentageLabel.text = "\(percentageProgress)%"
-        openLabel.text = "\(numberOfOpen ?? 0) open"
-        closedLabel.text = "\(numberOfOpen ?? 0) closed"
-
     }
     
 }
