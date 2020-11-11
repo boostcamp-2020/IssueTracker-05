7300c00b81d234157fa0e21e8eb62b9be0b085d0
seokminKang <soos0228@naver.com>
2020-11-11 18:42:27 +0900

    chore: 웹팩, 바벨설정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   resolve: {
     modules: [path.join(__dirname, 'src'), 'node_modules'],
