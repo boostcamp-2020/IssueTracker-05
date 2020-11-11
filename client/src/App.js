115a4c0bf4c8654bd7d10d1e0798b2a446e1e7e2
seokminKang <soos0228@naver.com>
2020-11-11 10:21:17 +0900

    feat: 사용하지 않는 선언제거로 루트 파일 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
-import Login from './temp/Login';
-import Title from './temp/Title';
-
 import theme from './components/themes/default';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Title />
-      <Login />
+      <div> hi </div>
     </ThemeProvider>
   );
 };
