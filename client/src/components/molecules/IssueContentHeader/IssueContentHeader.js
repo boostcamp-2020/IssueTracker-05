fe4f601ac609435070260a225ec37c9e698d9fae
seokminKang <soos0228@naver.com>
2020-11-12 21:38:02 +0900

    feat: 이슈 상세 페이지에서 헤더 컴포넌트 생성

---
+++ client/src/components/molecules/IssueContentHeader/IssueContentHeader.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Button from '@atoms/Button';
+import Span from '@atoms/Span';
+
+const WhoreWrapper = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #ccc;
+  padding: 0.5em;
+`;
+
+const InnerWrapper1 = styled.div`
+  box-sizing: border-box;
+  width: 30%;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+
+const InnerWrapper2 = styled.div`
+  box-sizing: border-box;
+  width: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 15px;
+`;
+
+const IssueContentHeader = (props) => {
+  const { issue } = props;
+  console.log(issue);
+  return (
+    <WhoreWrapper>
+      <InnerWrapper1>
+        <Span>{issue.user ? issue.user.nickname : ''}</Span>
+        <Span>
+          최종 수정:
+          {issue.createdAt ? issue.createdAt : ''}
+        </Span>
+      </InnerWrapper1>
+      <InnerWrapper2>
+        {props.writer && <Span>Owner</Span>}
+        <Button type="edit">Edit</Button>
+      </InnerWrapper2>
+    </WhoreWrapper>
+  );
+};
+
+IssueContentHeader.propTypes = {
+  writer: PropTypes.bool,
+};
+
+IssueContentHeader.defaultProps = {
+  title: '행복하다',
+  iid: 2,
+  closedAt: '최종 수정 날짜 입력하시오',
+  writer: true,
+};
+
+export default IssueContentHeader;
