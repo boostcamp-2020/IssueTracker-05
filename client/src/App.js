2430f3133ccb7f355cbfbc1991b2806fc3586633
seokminKang <soos0228@naver.com>
2020-11-12 21:31:24 +0900

    feat: 앱 라우터 연결

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
+
 import theme from './components/themes/default';
 
 import Main from './route/Main';
+// import Sign from './route/Sign';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Router>
+        {/* <Sign /> isAuthorized={isAuthorized}*/}
         <Main />
       </Router>
     </ThemeProvider>
